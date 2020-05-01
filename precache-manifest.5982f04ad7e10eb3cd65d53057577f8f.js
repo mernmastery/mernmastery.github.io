a457280f6cb7dad1b4eaed76d396fb88070ad7e9
mernmastery <mernmastery@gmail.com>
2020-05-02 04:31:24 +0530

    Initial commit

---
+++ precache-manifest.5982f04ad7e10eb3cd65d53057577f8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb7a4688c0a7ade3943185ac0d973371",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e7504ef7c9b7eba2d2e",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "762a5197e7af79933005",
+    "url": "/static/css/main.247ce16d.chunk.css"
+  },
+  {
+    "revision": "8e7504ef7c9b7eba2d2e",
+    "url": "/static/js/2.d18fc929.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d18fc929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "762a5197e7af79933005",
+    "url": "/static/js/main.44cb5f55.chunk.js"
+  },
+  {
+    "revision": "6e0487b5ba2b19467b7a",
+    "url": "/static/js/runtime-main.173e62e3.js"
+  }
+]);
